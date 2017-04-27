3193e8c9d31773b2173c94753fb2305b8e54b76b
Mihail Douhaniaris <mihail.douhaniaris@gmail.com>
2017-04-27 15:46:00 +0300

    Rearranged files.

--- src/r_fhmm.R
+++ src/r_fhmm.R
@@ -1,93 +1,92 @@
 # Load libraries
 library(ars)
-source("ffbs.R")
+source("src/ffbs.R")
+
+# Should each iteration be plotted?
+iter.plot <- T
+
+# Should negative appliances be allowed?
+allow.neg.thetas <- F
+
+# Color palette by Paul Tol
+tol5 <- c("#1B9E77", "#66A61E", "#7570B3", "#D95F02", "#E6AB02", "#E7298A")
 
 # Read file ----
-file <- read.csv("../in/mdr_2016-03_export_99.csv", sep = ";")
+file <- read.csv("test/test.csv")
 
-# Data preprocessing ----
-# Check number of different DeviceIDs
-if (length(unique(file$DeviceId)) > 1) {
-  warning("Multiple DeviceIDs in input file.")
+# Segment of file
+data <- file[6000:7400,]
+
+# Observed data
+Y <- data$aggregate
+
+# Add noise
+Y <- abs(Y + rnorm(length(Y), 0, 10))
+nc <- 10
+
+# Real values of parameters
+theta.real <- c(1000,2000,500,200)
+Z.real <- matrix(as.matrix(data[,c(-1,-6)]), ncol = 4)
+for (col in 1:ncol(Z.real)) {
+  Z.real[which(Z.real[,col] != 0),col] <- 1
 }
 
-# Store each phase to a separate data frame
-f1 <- file[which(file$TypeId == 1),]
-f2 <- file[which(file$TypeId == 2),]
-f3 <- file[which(file$TypeId == 3),]
-
-# Create data frame for data
-data <- data.frame(matrix(NA, length(unique(file$ValueTimeStamp)), 6))
-colnames(data) <- c("DeviceId", "ValueTimeStamp", "P1", "P2", "P3", "S")
-data[["DeviceId"]] <- file$DeviceId[1:nrow(data)]
-data[["ValueTimeStamp"]] <- unique(file$ValueTimeStamp)
-
-# Match each phase to its time
-# This is done only once so duplicates are ignored
-data[match(f1$ValueTimeStamp, data$ValueTimeStamp),"P1"] <- f1$DataValue
-data[match(f2$ValueTimeStamp, data$ValueTimeStamp),"P2"] <- f2$DataValue
-data[match(f3$ValueTimeStamp, data$ValueTimeStamp),"P3"] <- f3$DataValue
-data[,"S"] <- data[,"P1"] + data[,"P2"] + data[,"P3"]
-
-# Delete empty data rows
-data <- data[-which(is.na(data[,"S"])),]
-Y <- data$S # Observations
+# Plot observed signal and its components
+plot(Y, type = "l", ylab = "Power (W)", xlab = "Time (s)", col = tol5[1])
+lines(Z.real[,1]*theta.real[1], type = "l", lty = 1, col = tol5[2], lwd = 1.2)
+lines(Z.real[,2]*theta.real[2], type = "l", lty = 1, col = tol5[3], lwd = 1.2)
+lines(Z.real[,3]*theta.real[3], type = "l", lty = 1, col = tol5[4], lwd = 1.2)
+lines(Z.real[,4]*theta.real[4], type = "l", lty = 1, col = tol5[5], lwd = 1.2)
+legend("topleft", c("Aggregate","Oven","Heater","Fridge","Light"),
+       fill = c(tol5[1],tol5[2],tol5[3],tol5[4],tol5[5]))
 
 # Initialize NFHMM parameters ----
 # Hyperparameters
-# TODO: Conjugate priors for hyperparameters (hyperpriors)
-alpha <- 1.5 # mu, beta distribution, strength parameter of IBP
-gamma <- 1.5 # b, beta distribution
-delta <- 1 # b, beta distribution
-mu_theta <- mean(Y, na.rm = T)*2
+alpha <- 1
+gamma <- 1
+delta <- 1
+mu_theta <- mean(Y, na.rm = T)
+sigma_epsilon <- median(abs(diff(Y)))*nc
 sigma_theta <- sd(Y, na.rm = T)
-sigma_epsilon <- sd(Y, na.rm = T)
 
 # Parameters
-Kdag  <- 2 # Number of active appliances + 1
+Kdag <- 4 # Number of active appliances + 1
 Z <- matrix(1, length(Y), (Kdag - 1)) # State matrix
 Z[sample(1:length(Y), round(0.3*length(Y))),1] <- 0 # Add some zeros
 Z <- cbind(Z, matrix(0, nrow(Z), 1)) # Add empty column
 
-o.mu <- matrix(rbeta(Kdag, alpha/Kdag, 1), 1, Kdag) # State transition probability
-o.mu <- o.mu[order(o.mu, decreasing = T)] # Order state transition probabilities
-b <- matrix(rbeta(Kdag, gamma, delta), 1, Kdag) # State transition probability
-theta <- matrix(rnorm(Kdag, mu_theta, sigma_theta), 1, Kdag) # State levels
+mu <- rbeta(Kdag, alpha/Kdag, 1) # State transition probability
+mu <- mu[order(mu, decreasing = T)] # Order state trabsition probabilities
+b <- rbeta(Kdag, gamma, delta) # State transition probability
+theta <- rnorm(Kdag, mu_theta, sigma_theta) # State levels
 
 # Auxiliary functions ----
-# Indicator function (inclusive)
-indicator <- function(x, upb, lob = 0) {
-  if ((x >= lob) && (x <= upb)) {
-    return(1)
-  } else {
-    return(0)
-  }
-}
-
 # Padding log-distribution for mu_k
-fmuk <- function(x, alpha, t, N=10) {
+fmuk <- function(mu, alpha, t, N = 10) {
   sum <- 0
   for (i in 1:N) {
-    sum <- sum + 1/i*((1-x)^i)
+    sum <- sum + 1/i*((1-mu)^i)
   }
-  return((alpha*sum + t*log(1-x) + (alpha-1)*log(x)))    
+  return((alpha*sum + t*log(1-mu) + (alpha-1)*log(mu)))
 }
 
 # Derivative of padding log-distribution for mu_k
-dfmuk <- function(x, alpha, t, N=10) {
-  return(alpha*((1-x)^N-1)/x - t/(1-x) + (alpha-1)/x)
+dfmuk <- function(mu, alpha, t, N = 10) {
+  return(alpha*((1-mu)^N-1)/mu - t/(1-mu) + (alpha-1)/mu)
 }
+
 # log-functions for new mu's (ARS)
-bmuk <- function(x,ck00,ck01){
-  h <- ck00*log(1-x)+(ck01-1)*log(x)
+bmuk <- function(mu,ck00,ck01) {
+  h <- ck00*log(1-mu)+(ck01-1)*log(mu)
   return(h)
 }
-dbmuk <- function(x,ck00,ck01){
-  dh <- -ck00/(1-x)+(ck01-1)/x
+
+dbmuk <- function(mu,ck00,ck01) {
+  dh <- -ck00/(1-mu) + (ck01-1)/mu
   return(dh)
 }
 
-# Computation of c's
+# Computation of "c" constants
 cfun <- function(i, j, k, Z) {
   column <- Z[,k]
   i.inds <- which(column == i)
@@ -105,159 +104,273 @@ cfun <- function(i, j, k, Z) {
   return(c.val)
 }
 
+grid_sample_alpha <- function(mu,Kdag,lb=1,ub=5) {
+  
+  grid <- seq(lb,ub,by=0.001)
+  post <- rep(1,length(grid))
+  
+  for (i in 1:length(grid)) {
+    for (k in 1:Kdag) {
+      post[i] <- post[i]*dbeta(mu[k],grid[i]/Kdag,1)
+    }
+  }
+  
+  post <- post/sum(post)
+  cpost <- cumsum(post)
+  
+  u <- runif(1)
+  ind <- min(which(cpost > u))
+  #ind <- which.max(post) # Maximum likelihood method
+  return(grid[ind])
+}
+
+grid_sample_gamma <- function(b,Kdag,delta,lb=0,ub=5) {
+  
+  grid <- seq(lb,ub,by=0.001)
+  post <- rep(1,length(grid))
+  
+  for (i in 1:length(grid)) {
+    for (k in 1:Kdag) {
+      post[i] <- post[i]*dbeta(b[k],grid[i],delta)
+    }
+  }
+  
+  post <- post/sum(post)
+  cpost <- cumsum(post)
+  
+  u <- runif(1)
+  ind <- min(which(cpost > u))
+  #ind <- which.max(post) # Maximum likelihood method
+  return(grid[ind])
+}
+
+grid_sample_delta <- function(b,Kdag,gamma,lb=0,ub=5) {
+  
+  grid <- seq(lb,ub,by=0.001)
+  post <- rep(1,length(grid))
+  
+  for (i in 1:length(grid)) {
+    for (k in 1:Kdag) {
+      post[i] <- post[i]*dbeta(b[k],gamma,grid[i])
+    }
+  }
+  
+  post <- post/sum(post)
+  cpost <- cumsum(post)
+  
+  u <- runif(1)
+  ind <- min(which(cpost > u))
+  #ind <- which.max(post) # Maximum likelihood method
+  return(grid[ind])
+}
+
 # Iterative sampling for NFHMM ----
 # Number of iterations
-IterNum <- 1
+IterNum.total <- 100
+IterNum <- IterNum.total
 while (IterNum > 0) {
-  
   # K-active: The number of active appliances
-  Kact <- max(which(colSums(Z) > 0))
+  # Active appliances
+  ActApp <- which(colSums(Z) > 0)
+  if (length(ActApp) == 0) {
+    Kact <- 0
+  } else {
+    Kact <- max(ActApp)
+  }
+  
+  # Sample auxiliary variable s
+  # Define upper bounds (mu-star)
+  if (Kact == 0) {
+    ub <- 1
+  } else {
+    ub <- mu[Kact]
+  }
+  # Draw auxiliary variable s
+  s <- runif(1, 0, ub)
   
   # K-dagger: The index of the highest non-active appliance
   Kdag <- max(which(colSums(Z) == 0))
-
-  # Put mu in order
-  o.mu <- o.mu[order(o.mu, decreasing = T)]
   
-  # Sample auxiliary variable s
-  s <- runif(1, min = 0, max = o.mu[Kact])
+  # Put parameters in correct order
+  order.mu <- order(mu, decreasing = T)
+  mu <- mu[order.mu]
+  b <- b[order.mu]
+  theta <- theta[order.mu]
   
-  # K-star: The highest index for which o.mu[K-star] > s
-  Kstar <- max(which(o.mu > s))
+  # K-star: The highest index for which mu[K-star] > s
+  Kstar <- max(which(mu > s))
   
   # Expand representations
   while (Kstar >= Kdag) {
     # Sample mu_k with ARS
-    # Starting points for ARS
-    x <- runif(3, 0, o.mu[length(o.mu)])
-    x <- x[order(x)]
+    # Starting points and bounds for ARS
+    ub <- mu[length(mu)]
+    sp <- runif(4, 0, ub)
+    sp <- sp[order(sp)]
+    # Draw mu_k with ARS
+    mu_k <- ars(1, fmuk, dfmuk, x = sp, m = 4, lb = T, xlb = 0,
+                ub = T, xub = ub, alpha = alpha, t = nrow(Z))
     
-    # Sample mu_k
-    mu_k <- ars(1,fmuk,dfmuk,x=x,m=3,lb=T,xlb=0,ub=T,
-                xub=o.mu[length(o.mu)],alpha=alpha,t=nrow(Z))
+    # Sample b_k and theta_k from their priors
+    b_k <- rbeta(1, gamma, delta)
+    theta_k <- rnorm(1, mu_theta, sigma_theta) # TODO: Disallow negatives?
     
-    # Pad mu_k to ordered mu
-    o.mu <- c(o.mu, mu_k)
-    o.mu <- o.mu[order(o.mu, decreasing = T)]
+    # Pad sampled parameters to their vectors
+    mu <- c(mu, mu_k)
+    b <- c(b, b_k)
+    theta <- c(theta, theta_k)
+    
+    # Order parameter vectors
+    order.mu <- order(mu, decreasing = T)
+    mu <- mu[order.mu]
+    b <- b[order.mu]
+    theta <- theta[order.mu]
     
     # Add empty column to Z
     Z <- cbind(Z, matrix(0, nrow(Z), 1))
     
-    # Update K-star
-    Kstar <- max(which(o.mu > s))
-    
     # Update K-dagger
     Kdag <- max(which(colSums(Z) == 0))
     
-    # Sample b_k and theta_k from their priors
-    b <- c(b, rbeta(1, gamma, delta))
-    theta <- c(theta, rnorm(1, mu_theta, sigma_theta))
-    
+    # Update K-star
+    Kstar <- max(which(mu > s)) # TODO: Is this correct?
   }
   
   # Sample Z with blocked Gibbs and run FFBS on each column of Z
   # Note: Z is updated only up to column k <= K*
-  # Note: After sampling Z, for any columns k <= K* that are non-active, delete those columns
+  # Note: After sampling Z, for any columns k <= K* that are inactive, delete those columns
   for (k in 1:Kstar) {
-    W <- matrix(c(1-o.mu[k],o.mu[k],1-b[k],b[k]),byrow = T,nrow=2)
-
-    Z <- BSi(Y,Z,k,W,theta,sigma_epsilon,Kact,o.mu)
+    # Transition matrix for appliance k
+    W <- matrix(c(1-mu[k],mu[k],1-b[k],b[k]), byrow = T, nrow = 2)
+    # FFBS for column k
+    Z <- BSi(Y,Z,k,W,theta,sigma_epsilon,Kact,mu)
+    # Update number of active appliances
     Kact <- max(which(colSums(Z) > 0))
   }
-  
-  # Delete non-active columns for k <= K*
-  nonact <- which(colSums(matrix(Z[,1:Kstar], nrow = length(Y))) == 0)
-  if (length(nonact) > 0) {
-    Z <- Z[,-nonact]
-    o.mu <- o.mu[-nonact]
-    b <- b[-nonact]
-    theta <- theta[-nonact]
+  # Delete inactive columns for k <= K*
+  inact <- which(colSums(matrix(Z[,1:Kstar], nrow = length(Y))) == 0)
+  if (length(inact) > 0) {
+    Z <- Z[,-inact]
+    mu <- mu[-inact]
+    b <- b[-inact]
+    theta <- theta[-inact]
   }
   
   # Update K-dagger
   Kdag <- max(which(colSums(Z) == 0))
-
   
-  # Sample theta, mu, b, from their conditionals
-  # Sample theta
+  # Sample theta from its conditional
   for (k in 1:Kdag) {
     sigma_theta_p2 <- (1/sigma_theta^2 + 1/sigma_epsilon^2*sum(Z[,k]))^-1
     sum1 <- Z[,k]%*%Y
     thetadk <- theta[-k]
-    if (length(thetadk)==1){
-      sum2 <- Z[,-k] * thetadk
+    if (length(thetadk)==1) {
+      sum2 <- Z[,-k]*thetadk
     } else {
-      sum2 <- Z[,-k] %*% thetadk
+      sum2 <- Z[,-k]%*%thetadk
     }
-    
     sum2 <- sum(sum2*Z[,k])
     mu_theta_g <- sigma_theta_p2*((mu_theta/sigma_theta^2) + (sum1-sum2)/sigma_epsilon^2)
     sigma_theta_g <- sqrt(sigma_theta_p2)
-    theta[k] <- rnorm(1, mu_theta_g, sigma_theta_g)
+    theta_cand <- rnorm(1, mu_theta_g, sigma_theta_g)
+    if (allow.neg.thetas == F) {
+      while (theta_cand < 0.05*mean(Y)) {
+        theta_cand <- rnorm(1, mu_theta_g, sigma_theta)
+      }
+    }
+    theta[k] <- theta_cand
   }
-    
+  
   # Sample mu_k
   for (k in 1:(Kdag-1)) {
     # Find lower and upper bounds
     if (k == 1) {
-      ubmu <- 1
+      ub <- 1
     } else {
-      ubmu <- o.mu[k-1]
+      ub <- mu[k-1]
     }
-    if (is.na(o.mu[k+1])) {
-      lbmu <- 0
+    if (is.na(mu[k+1])) {
+      lb <- 0
     } else {
-      lbmu <- o.mu[k+1]
+      lb <- mu[k+1]
     }
     
-    # Compute c's
+    # Compute "c" constants
     ck00 <- cfun(0,0,k,Z)
     ck01 <- cfun(0,1,k,Z)
     
-    # Draw mu_k
-    x <- seq(lbmu,ubmu,length.out = 5)
-    x <- x[-c(1,5)]
-    o.mu[k] <- ars(n=1,bmuk,dbmuk,x=x,m=3,lb=T,xlb=lbmu,ub=T,xub=ubmu,ck00=ck00,ck01=ck01)
-    
-    # Draw mu_k using inverse grid sampling
-    # bound <- seq(lbmu, ubmu, by = 0.0001)
-    # dbound <- dbeta(bound,ck01,ck00+1)
-    # dbound <- dbound/sum(dbound)
-    # cbound <- cumsum(dbound)
-    # u <- runif(1)
-    # if (u <= cbound[1]) {
-    #   ind <- 1
-    # } else if (u >= cbound[length(cbound)]) {
-    #   ind <- length(cbound)
-    # } else {
-    #   ind <- min(which(cbound > u))
-    # }
-    # o.mu[k] <- bound[ind]
+    # Draw mu_k using ARS
+    # Starting points for ARS
+    sp <- seq(lb, ub, length.out = 15)
+    sp <- sp[-c(1,15)]
+    mu[k] <- ars(n = 1, bmuk, dbmuk, x = sp, m = 13, lb = T, xlb = lb,
+                 ub = T, xub = ub, ck00 = ck00, ck01 = ck01)
   }
   
-  # Sample mu_k for k = K-dagger (ARS)
-  # Starting points for ARS
-  x <- runif(3, 0, o.mu[Kdag-1])
-  x <- x[order(x)]
+  # Sample mu_K-dagger with ARS
+  # Starting point for ARS and upper bound
+  ub <- mu[Kdag-1]
+  sp <- runif(4, 0, ub)
+  sp <- sp[order(sp)]
   
-  # Sample mu_k
-  o.mu[Kdag] <- ars(1,fmuk,dfmuk,x=x,m=3,lb=T,xlb=0,ub=T,
-              xub=o.mu[Kdag-1],alpha=alpha,t=nrow(Z))
+  # Draw mu_K-dagger
+  mu[Kdag] <- ars(1, fmuk, dfmuk, x = sp, m = 4, lb = T, xlb = 0,
+                  ub = T, xub = ub, alpha = alpha, t = nrow(Z))
   
-    
   # Sample b_k
   for (k in 1:Kdag) {
-    # Compute c's
+    # Compute "c" constants
     ck11 <- cfun(1,1,k,Z)
     ck10 <- cfun(1,0,k,Z)
-    
     # Draw b_k
-    b[k] <- rbeta(1, ck11+gamma,ck10+1)
+    b[k] <- rbeta(1, ck11+gamma, ck10+delta+1)
   }
-    
   
-  # TODO: Sample alpha, gamma, delta, mu_theta, sigma_theta, sigma_epsilon from their posteriors (conjugacy)
+  # TODO: Sample hyperparameters sigma_theta, mu_theta
+  # from their posteriors (conjugacy)
+  alpha <- grid_sample_alpha(mu,Kdag)
+  gamma <- grid_sample_gamma(b,Kdag,delta)
+  delta <- grid_sample_delta(b,Kdag,gamma)
   
+  # This is a temporary heuristic workaround for conjugacy
+  m <- median(abs(diff(Y-Z%*%theta)))
+  sigma_epsilon <- abs(rnorm(1, m, 0.5*m))*nc
+  
+  # Decrease iteration counter
   IterNum <- IterNum - 1
+  
+  # Plot observed signal and its components for this iteration
+  if (iter.plot == T) {
+    Z.i <- Z[,-Kdag]
+    theta.i <- theta[-Kdag]
+    
+    try(computed.aggregate <- Z.i*theta.i, silent = T)
+    try(computed.aggregate <- Z.i%*%theta.i, silent = T)
+    plot(computed.aggregate, type = "l", ylab = "Power (W)", xlab = "Time (s)",
+         ylim = c(min(min(computed.aggregate), min(theta.i)), max(max(computed.aggregate), max(theta.i))),
+         col = tol5[1], main = paste(ncol(Z.i), "devices extracted")) 
+    if (is.null(ncol(Z.i)) == F) {
+      for (k in 1:ncol(Z.i)) {
+        lines(Z.i[,k]*theta.i[k], type = "l", lty = 1, col = tol5[(k+1)%%6], lwd = 1.2)
+      }
+    }
+  }
+}
 
+# Return represenations to MIBP by removing inactive appliances
+Z <- Z[,-Kdag]
+theta <- theta[-Kdag]
+
+print(paste("Retrieved", ncol(Z), "appliances."))
+
+# Plot observed signal and its components
+try(computed.aggregate <- Z*theta, silent = T)
+try(computed.aggregate <- Z%*%theta, silent = T)
+plot(computed.aggregate, type = "l", ylab = "Power (W)", xlab = "Time (s)",
+     ylim = c(min(min(computed.aggregate), min(theta)), max(max(computed.aggregate), max(theta))),
+     col = tol5[1], main = paste(ncol(Z), "devices extracted"))
+if (is.null(ncol(Z)) == F) {
+  for (k in 1:ncol(Z)) {
+    lines(Z[,k]*theta[k], type = "l", lty = 1, col = tol5[(k+1)%%6], lwd = 1.2)
+  }
 }
